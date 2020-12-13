e1e152bb057b02d5eb400449fb369b7a16fddcac
Thomas Zehnder <fly@zhs.ch>
2020-12-13 22:58:06 +0100

    Server adopted to run on host and local

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 // Require express and create an instance of it
+const path = require('path');
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
 
-var table_a = require('./table_a');
-var table_empty = require('./table_empty');
+var table_a = require('./server/table_a');
+var table_empty = require('./server/table_empty');
 
 var models = [];
 
 models['a'] = table_a;
+
+if (typeof(process.env.PASSENGER_BASE_URI)==='undefined'){
+    process.env.PASSENGER_BASE_URI = '';
+}
+console.log("process.env.PASSENGER_BASE_URI: ", process.env.PASSENGER_BASE_URI);
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -39,17 +45,18 @@ app.all('/services/*', function (req, res, next) {
 
 
 // Mount build folder
-app.use(express.static('./build'));
+const buildPath = path.join(__dirname, '/build');
+app.use(process.env.PASSENGER_BASE_URI, express.static(buildPath));
 
 
 // on the request to root (localhost:3001/)
-app.get('/test.txt', function (req, res) {
+app.get(process.env.PASSENGER_BASE_URI+'/test.txt', function (req, res) {
     res.setHeader('Content-type', 'text/plain');
     res.status(200).send('jass development test server');
 });
 
 // On localhost:3001/services/getnodeplay
-app.get('/services/getnodeplay', function (req, res) {
+app.get(process.env.PASSENGER_BASE_URI+'/services/getnodeplay', function (req, res) {
 
     var tablename = 'a';
 
@@ -67,7 +74,7 @@ app.get('/services/getnodeplay', function (req, res) {
 });
 
 // On localhost:3001/services/setnodeplay
-app.post('/services/setnodeplay', function (req, res) {
+app.post(process.env.PASSENGER_BASE_URI+'/services/setnodeplay', function (req, res) {
 
     var tablename = 'a';
 
@@ -83,7 +90,7 @@ app.post('/services/setnodeplay', function (req, res) {
 
 // Change the 404 message modifying the middleware
 app.use(function (req, res, next) {
-    res.status(404).send(`not implemented URL :-( ${req.originalUrl} )`);
+    res.status(404).send( `${buildPath} -->  not implemented URL :-( ${req.originalUrl} )`);
 });
 
 
